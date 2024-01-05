948c195923ba4af0aca6b3fbcdbdce9788778a39
Inderpreet Singh <inderpreet99@gmail.com>
2024-01-04 22:21:23 -0500

    sw: use settings/constants, avoid cache

--- sw.js
+++ sw.js
@@ -1,53 +1,66 @@
 const alarmName = "ailaan";
-const createAlarm = async (name) => {
-  await chrome.alarms.create(name, { periodInMinutes: 1 });
+const STORAGE_KEY = "ailaan-alarm-enabled";
+
+try { importScripts("constants.js"); } catch (e) { console.error(e); }
+
+const createAlarm = async (name, periodInMinutes = CONSTANTS.periodInMinutes) => {
+  await chrome.alarms.create(name, { periodInMinutes });
+  await chrome.storage.sync.set({[STORAGE_KEY]: 1});
 }
 
 // reset alarm on every start of SW
-const STORAGE_KEY = "ailaan-alarm-enabled";
-
-async function checkAlarmState() {
+const checkAlarmState = async () => {
   const { alarmEnabled } = await chrome.storage.sync.get(STORAGE_KEY);
+  const { periodInMinutes } = await getStorageOrConstant("periodInMinutes");
 
+  console.log("alarm check");
   if (alarmEnabled) {
     const alarm = await chrome.alarms.get(alarmName);
 
     if (!alarm) {
       console.log('creating alarm since it doesn\'t exist');
-      await createAlarm(alarmName);
+      await createAlarm(alarmName, periodInMinutes);
     }
+  } else {
+    console.log("must create new")
+    await createAlarm(alarmName, periodInMinutes);
   }
 }
-
 checkAlarmState();
 
-
-chrome.alarms.onAlarm.addListener((alarm) => {
-  console.log('fetching banner' + alarm.name);
+chrome.alarms.onAlarm.addListener(async (alarm) => {
+  console.log('fetching banner ' + alarm.name);
   if (alarm.name === alarmName) {
-    chrome.storage.sync.get("selectedPage", (data) => {
-      const selectedPage = data.selectedPage || "";
-      console.log("selectedPage", selectedPage);
-      if (selectedPage) {
-        fetchBanner(selectedPage);
-      }
-    });
+    const {selectedPage} = await getStorageOrConstant("selectedPage");
+
+    if (selectedPage) {
+      await fetchAilaan(selectedPage);
+    }
   }
 });
-function fetchBanner(selectedPage) {
-  fetch("https://dev.bostonsikhsangat.com/wp-json/ailaan/v1/get")
+
+const fetchAilaan = async (selectedPage) => {
+  const { ailaanApi } = await getStorageOrConstant("ailaanApi");
+  fetch(ailaanApi, {cache: "no-cache"})
     .then(response => response.json())
     .then(data => {
       console.log("data", data)
-      // chrome.storage.local.set({ bannerData });
       if (data.message) {
         chrome.tabs.query({ url: selectedPage }, (tabs) => {
           tabs.forEach(tab => {
-            console.log("tab selected", tab);
-            chrome.tabs.sendMessage(tab.id, { banner: data.message });
+            chrome.tabs.sendMessage(tab.id, { ailaan: data.message });
           });
         });
       }
     })
-    .catch(error => console.error("Error fetching banner:", error));
+    .catch(error => console.error("Error fetching ailaan:", error));
+}
+
+const getStorageOrConstant = async (setting) => {
+  const fetchedData = await chrome.storage.sync.get(setting);
+  if (fetchedData[setting]) {
+    return fetchedData;
+  }
+
+  return CONSTANTS[setting];
 }
