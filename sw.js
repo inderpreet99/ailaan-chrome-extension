3bb250af04de5246f956696511154f75a340c28c
Inderpreet Singh <inderpreet99@gmail.com>
2024-01-05 09:26:25 -0500

    Handle for missing tabs

--- sw.js
+++ sw.js
@@ -1,28 +1,28 @@
 const alarmName = "ailaan";
-const STORAGE_KEY = "ailaan-alarm-enabled";
 
 try { importScripts("constants.js"); } catch (e) { console.error(e); }
 
 const createAlarm = async (name, periodInMinutes = CONSTANTS.periodInMinutes) => {
   await chrome.alarms.create(name, { periodInMinutes });
-  await chrome.storage.sync.set({[STORAGE_KEY]: 1});
+}
+
+const getStorageOrConstant = async (setting) => {
+  const fetchedData = await chrome.storage.sync.get(setting);
+  if (fetchedData[setting]) {
+    return fetchedData;
+  }
+
+  return CONSTANTS[setting];
 }
 
 // reset alarm on every start of SW
 const checkAlarmState = async () => {
-  const { alarmEnabled } = await chrome.storage.sync.get(STORAGE_KEY);
   const { periodInMinutes } = await getStorageOrConstant("periodInMinutes");
 
-  console.log("alarm check");
-  if (alarmEnabled) {
-    const alarm = await chrome.alarms.get(alarmName);
+  const alarm = await chrome.alarms.get(alarmName);
 
-    if (!alarm) {
-      console.log('creating alarm since it doesn\'t exist');
-      await createAlarm(alarmName, periodInMinutes);
-    }
-  } else {
-    console.log("must create new")
+  if (!alarm) {
+    console.log('creating alarm since it doesn\'t exist');
     await createAlarm(alarmName, periodInMinutes);
   }
 }
@@ -47,6 +47,10 @@ const fetchAilaan = async (selectedPage) => {
       console.log("data", data)
       if (data.message) {
         chrome.tabs.query({ url: selectedPage }, (tabs) => {
+          if (!tabs) {
+            console.warn("Tab is not open!")
+            return
+          }
           tabs.forEach(tab => {
             chrome.tabs.sendMessage(tab.id, { ailaan: data.message });
           });
@@ -54,13 +58,4 @@ const fetchAilaan = async (selectedPage) => {
       }
     })
     .catch(error => console.error("Error fetching ailaan:", error));
-}
-
-const getStorageOrConstant = async (setting) => {
-  const fetchedData = await chrome.storage.sync.get(setting);
-  if (fetchedData[setting]) {
-    return fetchedData;
-  }
-
-  return CONSTANTS[setting];
 }
