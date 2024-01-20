84c41687cfbdceb503b2fa8fba3d5d58b4c225f3
Inderpreet Singh <inderpreet99@gmail.com>
2024-01-20 15:37:56 -0500

    Set css

--- content.js
+++ content.js
@@ -1,21 +1,44 @@
+const AILAAN_DIV = "ailaan";
+const AILAAN_STYLE = "ailaanStyle";
+
 const getAilaanDiv = () => {
+    // Find ailaanDiv and create if it doesn't exist
 
     // find ailaan
-    const ailaanDiv = document.getElementById("ailaan")
+    const ailaanDiv = document.getElementById(AILAAN_DIV)
     if (ailaanDiv) {
         return ailaanDiv;
     }
 
     // ailaan not on page, let's add
     const newDiv = document.createElement("div");
-    newDiv.setAttribute("id", "ailaan")
+    newDiv.setAttribute("id", AILAAN_DIV)
     return newDiv;
 }
 
-const setClass = (ailaanDiv, className) => {
+const getAilaanStyle = () => {
+    // Create Ailaan Style and add to head if it doesn't exist
+
+    // find style
+    const ailaanStyle = document.getElementById(AILAAN_STYLE)
+    if (ailaanStyle) {
+        return ailaanStyle;
+    }
+
+    // style not on page, let's create
+    const newStyle = document.createElement("style");
+    newStyle.setAttribute("type", "text/css");
+    newStyle.setAttribute("id", AILAAN_STYLE);
+    document.head.appendChild(newStyle);
+    return newStyle;
+}
+
+const createAilaan = (ailaanDiv, className) => {
+    // Create ailaanDiv appended within className'd node.
+
     var targetElement = document.body;
-    if (message.targetClass) {
-        elements = document.getElementsByClassName(message.targetClass);
+    if (className) {
+        elements = document.getElementsByClassName(className);
         if (elements) {
             targetElement = elements[0];
         }
@@ -27,12 +50,18 @@ const setMessage = (ailaanDiv, ailaan) => {
     ailaanDiv.innerHTML = ailaan;
 }
 
+const setCss = (targetCss) => {
+    const ailaanStyle = getAilaanStyle();
+    ailaanStyle.innerHTML = targetCss;
+}
+
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.ailaan) {
-        console.log("message received" , message);
+        console.debug("message received" , message);
 
         const ailaanDiv = getAilaanDiv();
         setMessage(ailaanDiv, message.ailaan);
-        setClass(ailaanDiv, message.targetClass)
+        createAilaan(ailaanDiv, message.targetClass)
+        setCss(message.targetCss);
     }
 });
